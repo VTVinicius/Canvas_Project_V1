fc278e5b5217308130b9c06d0b77063c5a4e6d0a
VTVinicius <53088125+VTVinicius@users.noreply.github.com>
2022-12-06 08:31:00 -0300

    Animated path

--- app/src/main/java/br/com/vtvinicius/canvasprojectv1/MainActivity.kt
+++ app/src/main/java/br/com/vtvinicius/canvasprojectv1/MainActivity.kt
@@ -3,6 +3,10 @@ package br.com.vtvinicius.canvasprojectv1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import br.com.vtvinicius.canvasprojectv1.lessons.lesson10.AnimatedArrow
+import br.com.vtvinicius.canvasprojectv1.lessons.lesson10.AnimatedCircle
+import br.com.vtvinicius.canvasprojectv1.lessons.lesson10.AnimatedPath
+import br.com.vtvinicius.canvasprojectv1.lessons.lesson10.AnimatedScreen
 import br.com.vtvinicius.canvasprojectv1.lessons.lesson9.PathOperation
 import br.com.vtvinicius.canvasprojectv1.navigation.LessonsNavigationController
 import br.com.vtvinicius.canvasprojectv1.ui.theme.ApplicationTheme
@@ -12,7 +16,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ApplicationTheme {
-                LessonsNavigationController()
+                AnimatedScreen()
             }
         }
     }
---
+++ app/src/main/java/br/com/vtvinicius/canvasprojectv1/lessons/lesson10/AnimatedScreen.kt
@@ -0,0 +1,12 @@
+package br.com.vtvinicius.canvasprojectv1.lessons.lesson10
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AnimatedScreen() {
+
+    AnimatedPath()
+    AnimatedArrow()
+    AnimatedCircle()
+
+}
