96c4b0acfba33f78bbe765f7af56117740e6d125
VTVinicius <53088125+VTVinicius@users.noreply.github.com>
2022-12-06 07:46:53 -0300

    DP TO PX

--- app/src/main/java/br/com/vtvinicius/canvasprojectv1/lessons/lesson9/PathOperations.kt
+++ app/src/main/java/br/com/vtvinicius/canvasprojectv1/lessons/lesson9/PathOperations.kt
@@ -28,31 +28,31 @@ fun PathOperation() {
         Canvas(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(2000.dp)
+                .height(700.dp)
         ) {
             val squareWithoutOp1 = Path().apply {
-                addRect(Rect(Offset(200f, 200f), Size(200f, 200f)))
+                addRect(Rect(Offset(70.dp.toPx(), 70.dp.toPx()), Size(70.dp.toPx(), 70.dp.toPx())))
             }
             val circle1 = Path().apply {
-                addOval(Rect(Offset(200f, 200f), 100f))
+                addOval(Rect(Offset(70.dp.toPx(), 70.dp.toPx()), 35.dp.toPx()))
             }
             val squareWithoutOp2 = Path().apply {
-                addRect(Rect(Offset(200f, 600f), Size(200f, 200f)))
+                addRect(Rect(Offset(70.dp.toPx(), 200.dp.toPx()), Size(70.dp.toPx(), 70.dp.toPx())))
             }
             val circle2 = Path().apply {
-                addOval(Rect(Offset(200f, 600f), 100f))
+                addOval(Rect(Offset(70.dp.toPx(), 200.dp.toPx()), 35.dp.toPx()))
             }
             val squareWithoutOp3 = Path().apply {
-                addRect(Rect(Offset(200f, 1000f), Size(200f, 200f)))
+                addRect(Rect(Offset(70.dp.toPx(), 330.dp.toPx()), Size(70.dp.toPx(), 70.dp.toPx())))
             }
             val circle3 = Path().apply {
-                addOval(Rect(Offset(200f, 1000f), 100f))
+                addOval(Rect(Offset(70.dp.toPx(), 330.dp.toPx()), 35.dp.toPx()))
             }
             val squareWithoutOp4 = Path().apply {
-                addRect(Rect(Offset(200f, 1400f), Size(200f, 200f)))
+                addRect(Rect(Offset(70.dp.toPx(), 460.dp.toPx()), Size(70.dp.toPx(),70.dp.toPx() )))
             }
             val circle4 = Path().apply {
-                addOval(Rect(Offset(200f, 1400f), 100f))
+                addOval(Rect(Offset(70.dp.toPx(), 460.dp.toPx()), 35.dp.toPx()))
             }
             val pathWithOp1 = Path().apply {
                 op(squareWithoutOp1, circle1, PathOperation.Difference)
