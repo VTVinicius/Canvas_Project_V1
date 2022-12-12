0af387de51be06528aa121437b4eaa67934f5b53
VTVinicius <53088125+VTVinicius@users.noreply.github.com>
2022-12-12 13:57:00 -0300

    alterações no grafico

--- app/src/main/java/br/com/vtvinicius/canvasprojectv1/projects/line_graph/LineGraphScreen.kt
+++ app/src/main/java/br/com/vtvinicius/canvasprojectv1/projects/line_graph/LineGraphScreen.kt
@@ -9,20 +9,26 @@ fun LineGraphScreen() {
     val pointsList = mutableListOf<Points>()
 
     //Add 10 items to the Points list
-    pointsList.add(Points(valueX = 100f, valueY = 300f))
-    pointsList.add(Points(valueX = 150f, valueY = 100f))
-    pointsList.add(Points(valueX = 200f, valueY = 300f))
-    pointsList.add(Points(valueX = 250f, valueY = 100f))
-    pointsList.add(Points(valueX = 300f, valueY = 300f))
-    pointsList.add(Points(valueX = 350f, valueY = 100f))
-    pointsList.add(Points(valueX = 400f, valueY = 300f))
-    pointsList.add(Points(valueX = 450f, valueY = 100f))
-    pointsList.add(Points(valueX = 500f, valueY =300f))
-    pointsList.add(Points(valueX = 550f, valueY =100f))
+    pointsList.add(Points( valueY = 300f))
+    pointsList.add(Points( valueY = 100f))
+    pointsList.add(Points( valueY = 300f))
+    pointsList.add(Points(valueY = 100f))
+    pointsList.add(Points( valueY = 300f))
+    pointsList.add(Points( valueY = 100f))
+    pointsList.add(Points( valueY = 300f))
+    pointsList.add(Points( valueY = 100f))
+    pointsList.add(Points( valueY =300f))
+    pointsList.add(Points( valueY =100f))
+    pointsList.add(Points( valueY =300f))
+    pointsList.add(Points( valueY =100f))
+    pointsList.add(Points( valueY =300f))
+    pointsList.add(Points( valueY =100f))
+    pointsList.add(Points( valueY =300f))
+    pointsList.add(Points( valueY =100f))
 
 
     //oganize the list by the X value in ascending order
-    pointsList.sortBy { it.valueX }
+
 
 
     LineGraph(pointsList = pointsList)
--- app/src/main/java/br/com/vtvinicius/canvasprojectv1/projects/line_graph/Points.kt
+++ app/src/main/java/br/com/vtvinicius/canvasprojectv1/projects/line_graph/Points.kt
@@ -1,6 +1,5 @@
 package br.com.vtvinicius.canvasprojectv1.projects.line_graph
 
 data class Points(
-    var valueX: Float,
     var valueY: Float
 )
